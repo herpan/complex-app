d0f07d95d6a84ec5af72aa1a52982b28465a69fd
herpan <herpan@valista.co.id>
2025-02-10 16:27:04 +0700

    First Commit

---
+++ app/Models/User.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class User extends Authenticatable implements JWTSubject
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'password' => 'hashed',
+        ];
+    }
+
+    // Implement JWTSubject Methods
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    //Relasi ke Post
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class, 'user_id');
+    }
+}
+
---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
+
+
+
+//Route Soal No. 5
+Route::post('/users', [UserController::class, 'store']);
+
+//Route Soal No. 10
+Route::post('/login', [UserController::class, 'login']);
+
+
+Route::middleware('jwt.auth')->group(function () {
+    //Route Soal No. 6
+    Route::post('/posts', [PostController::class, 'store']);
+    
+    //Route Soal No. 7
+    Route::get('/users/{id}/posts', [UserController::class, 'userPosts']);
+    
+    //Route Soal No. 8
+    Route::get('/posts', [PostController::class, 'index']);
+
+    //Route Soal No. 11
+    Route::delete('/users/{id}', [UserController::class, 'destroy']);
+});
